365d676dfd46ec99225fb4cc8895762b8d297397
轩辕焮痕 <hzz780@qq.com>
2019-04-04 16:01:49 +0800

    Update inject.js

--- app/web/inject.js
+++ app/web/inject.js
@@ -149,11 +149,7 @@ class NightAElf {
                 }
             }).then(result => {
                 const message = JSON.parse(result.message);
-                // const methods = message.abi.Methods;
-                // const methods = Object.keys(message.service.methods);
-                // const methods = Object.keys(message.service.methods);
 
-                // const methods = [];
                 let contractMethods = {};
                 message.services.map(item => {
                     const methods = Object.keys(item.methods);
@@ -166,15 +162,6 @@ class NightAElf {
                         };
                     });
                 });
-                // let contractMethods = {};
-                // methods.map(item => {
-                //     contractMethods[item] = (...params) => {
-                //         _callAelfContract(params, 'CALL_AELF_CONTRACT', contractAddress, item);
-                //     };
-                //     contractMethods[item].call = (...params) => {
-                //         _callAelfContract(params, 'CALL_AELF_CONTRACT_READONLY', contractAddress, item);
-                //     };
-                // });
                 callback(null, contractMethods);
             });
         };
